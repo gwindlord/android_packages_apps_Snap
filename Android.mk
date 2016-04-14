gwindlord/android_packages_apps_SnapdragonCamera

Making libRawToDng part of the other system, apart of the cam
Change-Id: Iee30572adca3788aa00b150869db5fe53a3bcc5f

Android.mk
@@ -34,12 +34,10 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 # the libraries in the APK, otherwise just put them in /system/lib and
 # leave them out of the APK
 ifneq (,$(TARGET_BUILD_APPS))
-  LOCAL_JNI_SHARED_LIBRARIES := libjni_snapcammosaic libjni_snapcamtinyplanet
+  LOCAL_JNI_SHARED_LIBRARIES := libjni_snapcammosaic libjni_snapcamtinyplanet libRawToDng
 else
-  LOCAL_REQUIRED_MODULES := libjni_snapcammosaic libjni_snapcamtinyplanet
+  LOCAL_REQUIRED_MODULES := libjni_snapcammosaic libjni_snapcamtinyplanet libRawToDng
 endif
-
-LOCAL_PREBUILT_JNI_LIBS := libs/libRawToDng.so
 
 include $(BUILD_PACKAGE)
 

gwindlord/android_packages_apps_Snap

Conflicts:
	Android.mk

Android.mk
@@ -37,12 +37,10 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 # the libraries in the APK, otherwise just put them in /system/lib and
 # leave them out of the APK
 ifneq (,$(TARGET_BUILD_APPS))
-  LOCAL_JNI_SHARED_LIBRARIES := libjni_snapmosaic libjni_snaptinyplanet
+  LOCAL_JNI_SHARED_LIBRARIES := libjni_snapmosaic libjni_snaptinyplanet libRawToDng
 else
-  LOCAL_REQUIRED_MODULES := libjni_snapmosaic libjni_snaptinyplanet
+  LOCAL_REQUIRED_MODULES := libjni_snapmosaic libjni_snaptinyplanet libRawToDng
 endif
-
-LOCAL_PREBUILT_JNI_LIBS := libs/libRawToDng.so
 
 include $(BUILD_PACKAGE)
 