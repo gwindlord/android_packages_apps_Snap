1a286d5585fe7c73b679f386d42e24f3fded0b4b
gwindlord <gwindlord@users.noreply.github.com>
2016-05-03 08:24:06 +0200

    Revert "Denoise option in Camcorder"
    This reverts commit a2148174042c01fee8c36ccaea666f738f169290.

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -847,9 +847,8 @@ public class VideoModule implements CameraModule,
 
         // Set wavelet denoise mode
         if (mParameters.getSupportedDenoiseModes() != null) {
-            String Denoise = mPreferences.getString( CameraSettings.KEY_DENOISE,
-                             mActivity.getString(R.string.pref_camera_denoise_default));
-            mParameters.setDenoise(Denoise);
+            mParameters.setDenoise("denoise-on");
+        }
     }
 
     private final class AutoFocusCallback
